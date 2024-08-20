0b952c9a50cf009c240cf09302e42080033478ca
w3schools-test <paruisourabh4622@gmail.com>
2024-08-20 10:26:46 +0530

    modified and corrected Queue.c program

--- DSA_NOTES_MISS/Queue.c
+++ DSA_NOTES_MISS/Queue.c
@@ -1,74 +1,98 @@
-#include<stdio.h>
-#include<conio.h>
-int queue[5],f = -1, n = -1;
+#include <stdio.h>
+#include <stdlib.h> // Added for exit function
+
+int queue[5], f = -1, r = -1;  // Fixed 'n' to 'r' for rear
+
 void rear();
 void front();
 void show();
-void main()
+
+int main()  // Fixed 'main' function declaration
 {
     int ch;
-    clrscr();
-    printf("1.REAR\n");
-    printf("2.FRONT\n");
-    printf("3.SHOW\n");
-    printf("4.EXIT\n");
+    clrscr();  // Fixed typo: should be clrscr() not clrser()
+    
+    printf("1. REAR\n");
+    printf("2. FRONT\n");
+    printf("3. SHOW\n");
+    printf("4. EXIT\n");
+
     while(1)
     {
         printf("Enter your Choice: ");
-        scanf("%d",&ch);
+        scanf("%d", &ch);
+
         switch(ch)
         {
-            case 1:rear();
+            case 1:
+                rear();
                 break;
-            case 2:front();
+            case 2:
+                front();
                 break;
-            case 3:show();
+            case 3:
+                show();
                 break;
-            case 4:exit(0);
-                break;
-            default:printf("Invalid Choice\n");
+            case 4:
+                exit(0);  // Exit the program
+            default:
+                printf("Invalid Choice\n");
         }
     }
 }
-void rear()
+
+void rear()  // Correct function name for enqueue operation
 {
     int item;
-    if (r == 5-1)
+    if (r == 5 - 1)
     {
         printf("Queue is full\n");
     }
-    else{
+    else
+    {
         if(f == -1)
         {
-            f = 0;
+            f = 0;  // Initialize front if queue is empty
         }
         printf("Insert Element in Queue: ");
-        scanf("%d",&item);
-        r = r+1;
+        scanf("%d", &item);
+        r = r + 1;
         queue[r] = item;
     }
 }
-void front()
+
+void front()  // Correct function name for dequeue operation
 {
-    if(f == -1)
+    if(f == -1 || f > r)
     {
         printf("Queue is empty\n");
     }
-    else{
-        printf("Deleted %d",queue[f]);
-        f = f+1;
+    else
+    {
+        printf("Deleted %d\n", queue[f]);
+        f = f + 1;
+
+        if(f > r)  // Reset queue when all elements are dequeued
+        {
+            f = r = -1;
+        }
     }
 }
+
 void show()
 {
     int i;
     if(f == -1)
-    printf("Queue is empty");
-    else{
-        printf("Queue elements in: ");
-        for(i=f;i<=r;i++)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        printf("Queue elements are: ");
+        for(i = f; i <= r; i++)
         {
-            printf("%d ",queue[i]);
+            printf("%d ", queue[i]);
         }
+        printf("\n");
     }
 }
